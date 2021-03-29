db50493a18c3b92af50ef13d8f29e2c566959e1a
Jason Ertel <jason.ertel@securityonionsolutions.com>
2021-03-29 17:41:02 -0400

    Change Fleet to Fleet DM; Improve login form timeout handling (now redirects back to new login form); Begin websocket automated management implementation

---
+++ web/websockethandler_test.go
@@ -0,0 +1,30 @@
+// Copyright 2019 Jason Ertel (jertel). All rights reserved.
+// Copyright 2020-2021 Security Onion Solutions, LLC. All rights reserved.
+//
+// This program is distributed under the terms of version 2 of the
+// GNU General Public License.  See LICENSE for further details.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+package web
+
+import (
+  "testing"
+  "time"
+)
+
+func TestHandlePingMessage(tester *testing.T) {
+	webSocketHandler := NewWebSocketHandler(nil)
+	conn := NewConnection(nil)
+	oldPingTime := conn.lastPingTime
+	time.Sleep(3 * time.Millisecond)
+	msg := &WebSocketMessage{ Kind: "Ping" }
+	webSocketHandler.handleMessage(msg, conn)
+	newPingTime := conn.lastPingTime
+
+	if newPingTime.Sub(oldPingTime).Milliseconds() < 3 {
+		tester.Errorf("expected increase in lastPingTime from %v, but got %v", oldPingTime, newPingTime)
+	}
+}
