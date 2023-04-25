f29a90a9117ac68d1e0ec2a5b5481d28303be3ad
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2023-04-25 11:55:04 -0600

    Fixed bug, refactored test
    Because the home route was recreating papi, our tests couldn't test it effectively. Fixed the bug and refactored the tests around the new implementation.

--- html/js/routes/home.test.js
+++ html/js/routes/home.test.js
@@ -1,49 +1,38 @@
 require('../test_common.js');
 require('./home.js');
 
-let home = getComponent('home');
+let comp;
+
+beforeEach(() => {
+  comp = getComponent("home");
+  resetPapi();
+});
 
 test('loadChanges', async () => {
-  // save original functions
-  const createApi = home.$root.createApi;
   const showErrorMock = mockShowError();
   const data = 'MOTD';
-
-  // mock functions for sunny day path
-  const mockGoodGetter = jest.fn().mockReturnValue({data: data});
-  home.$root.createApi = jest.fn().mockReturnValue({
-    get: mockGoodGetter,
-  });
+  var mock = mockPapi('get', { data: data });
 
   // test
-  await home.loadChanges();
+  await comp.loadChanges();
 
   // verify
   expect(showErrorMock).toHaveBeenCalledTimes(0);
-  expect(home.$root.createApi).toHaveBeenCalledTimes(1);
-  expect(mockGoodGetter).toHaveBeenCalledTimes(1);
-  expect(mockGoodGetter).toHaveBeenCalledWith(expect.stringMatching(/motd.md\?v=\d+/));
-  expect(home.motd).toBe(data);
+  expect(mock).toHaveBeenCalledTimes(1);
+  expect(mock).toHaveBeenCalledWith(expect.stringMatching(/motd.md\?v=\d+/));
+  expect(comp.motd).toBe(data);
+});
 
-  // reset
-  home.motd = '';
-
-  // mock functions for rainy day path
-  const mockBadGetter = jest.fn().mockImplementation(() => { throw new Error() });
-  home.$root.createApi = jest.fn().mockReturnValue({
-    get: mockBadGetter,
-  });
+test('loadChanges error', async () => {
+  const showErrorMock = mockShowError();
+  mock = mockPapi('get', null, new Error('test error'));
 
   // test
-  await home.loadChanges();
+  await comp.loadChanges();
 
   // verify
+  expect(mock).toHaveBeenCalledTimes(1);
+  expect(mock).toHaveBeenCalledWith(expect.stringMatching(/motd.md\?v=\d+/));
   expect(showErrorMock).toHaveBeenCalledTimes(1);
-  expect(home.$root.createApi).toHaveBeenCalledTimes(1);
-  expect(mockBadGetter).toHaveBeenCalledTimes(1);
-  expect(mockBadGetter).toHaveBeenCalledWith(expect.stringMatching(/motd.md\?v=\d+/));
-  expect(home.motd).toBe('');
-
-  // restore original functions
-  home.$root.createApi = createApi;
+  expect(comp.motd).toBe('');
 });
