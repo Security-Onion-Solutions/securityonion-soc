a05fce1bfc2524c0314c3aeabbe7ca4f3e752b48
Josh Brower <Josh@DefensiveDepth.com>
2024-02-13 13:38:21 -0500

    Dont enable Sigma rules upon import

--- server/modules/elastalert/elastalert.go
+++ server/modules/elastalert/elastalert.go
@@ -433,7 +433,7 @@ func (e *ElastAlertEngine) syncCommunityDetections(ctx context.Context, detectio
 				results.Unchanged++
 			}
 		} else {
-			det.IsEnabled = true
+			det.IsEnabled = false
 
 			_, err = e.srv.Detectionstore.CreateDetection(ctx, det)
 			if err != nil {
