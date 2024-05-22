75cedd166bc5d55dc86cf72bff56a10e132e95f3
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2024-05-22 12:47:50 -0600

    Update Test
    When modifying suricata rules, the regex and replacement value are both double quoted. The test now reflects that.

--- server/modules/suricata/suricata_test.go
+++ server/modules/suricata/suricata_test.go
@@ -657,7 +657,7 @@ func TestSyncLocalSuricata(t *testing.T) {
 				"idstools.rules.local__rules":      SimpleRule,
 				"idstools.sids.enabled":            SimpleRuleSID,
 				"idstools.sids.disabled":           "",
-				"idstools.sids.modify":             SimpleRuleSID + " rev:7; rev:8;",
+				"idstools.sids.modify":             SimpleRuleSID + ` "rev:7;" "rev:8;"`,
 				"suricata.thresholding.sids__yaml": "\"10000\":\n    - modify:\n        regex: rev:7;\n        value: rev:8;\n",
 			},
 		},
