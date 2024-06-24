64b8c7500ef889c06ea5b40c33246111d7539e8e
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2024-06-24 12:58:54 -0600

    Updated Existing And Added Missing License Comments
    Adds copyright comments to a handful of files that were missing them and updated existing copyright comments for the year 2024.

--- server/modules/suricata/validate_test.go
+++ server/modules/suricata/validate_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020-2023 Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
+// Copyright 2020-2024 Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
 // or more contributor license agreements. Licensed under the Elastic License 2.0 as shown at
 // https://securityonion.net/license; you may not use this file except in compliance with the
 // Elastic License 2.0.
