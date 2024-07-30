6f076ac522f812d65e6b35a84e42e92cb76df527
Jason Ertel <jason.ertel@securityonionsolutions.com>
2024-07-30 15:06:01 -0400

    replace final setting term with title, if avail

--- html/js/routes/config.test.js
+++ html/js/routes/config.test.js
@@ -689,6 +689,9 @@ test('getSettingBreadcrumbs', () => {
   setting = { id: "foo.bar.car", advanced: false, extended: false };
   expect(comp.getSettingBreadcrumbs(setting)).toBe("foo > bar > car");
 
+  setting = { id: "foo.bar.car", advanced: false, extended: false, title: "Carbine" };
+  expect(comp.getSettingBreadcrumbs(setting)).toBe("foo > bar > Carbine");
+
   setting = { id: "foo.bar.car", advanced: false, extended: true };
   expect(comp.getSettingBreadcrumbs(setting)).toBe("foo > bar > car [ext]");
 
