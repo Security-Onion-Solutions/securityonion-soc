de4d99444891709c87fe3cd3d7d741f022fcdc77
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2024-11-11 10:28:11 -0700

    Better Explain Invalid Note Field When Over Max Length

--- html/js/i18n.js
+++ html/js/i18n.js
@@ -598,6 +598,7 @@ const i18n = {
       markdownFormattingSupported: 'Markdown formatting supported',
       maximize: 'Maximize View (ESC to cancel)',
       maxUploadSize: 'Maximum upload size',
+      maxLengthFormat: 'Max length is {length} characters',
       mbps: 'Mb/s',
       md5: 'MD5',
       memUsage: 'Memory Usage',
