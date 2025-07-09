e7827f0e936e78647ad9023af4ca050aa882f908
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2025-07-09 11:51:48 -0600

    New Module Basics

---
+++ model/assistant.go
@@ -0,0 +1,23 @@
+// Copyright 2020-2025 Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0 as shown at
+// https://securityonion.net/license; you may not use this file except in compliance with the
+// Elastic License 2.0.
+
+package model
+
+type ChatRequest struct {
+	Messages      []*ChatMessage `json:"messages"`
+	Model         string         `json:"model,omitempty"`
+	MaxTokens     int            `json:"max_tokens,omitempty"`
+	Temperature   float32        `json:"temperature,omitempty"`
+	TopP          float32        `json:"top_p"`
+	TopK          int            `json:"top_k"`
+	StopSequences []string       `json:"stop_sequences,omitempty"`
+	System        string         `json:"system,omitempty"`
+	UserUUID      string         `json:"user_uuid,omitempty"`
+}
+
+type ChatMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
---
+++ server/modules/assistant/assistant.go
@@ -0,0 +1,61 @@
+// Copyright 2020-2025 Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0 as shown at
+// https://securityonion.net/license; you may not use this file except in compliance with the
+// Elastic License 2.0.
+
+package assistant
+
+import (
+	"github.com/security-onion-solutions/securityonion-soc/module"
+	"github.com/security-onion-solutions/securityonion-soc/server"
+)
+
+const (
+	DEFAULT_APIKEY = ""
+	DEFAULT_APIURL = "https://b7cdq33i55t73bzisq7gplzd340mvxgo.lambda-url.us-east-2.on.aws/"
+	DEFAULT_MODEL  = "claude-sonnet"
+)
+
+type AssistantManager struct {
+	srv       *server.Server
+	apiKey    string
+	apiUrl    string
+	model     string
+	isRunning bool
+}
+
+func NewAssistantManager(srv *server.Server) *AssistantManager {
+	return &AssistantManager{
+		srv: srv,
+	}
+}
+
+func (am *AssistantManager) PrerequisiteModules() []string {
+	return nil
+}
+
+func (am *AssistantManager) Init(config module.ModuleConfig) (err error) {
+	am.apiKey = module.GetStringDefault(config, "apiKey", DEFAULT_APIKEY)
+	am.apiUrl = module.GetStringDefault(config, "apiUrl", DEFAULT_APIURL)
+	am.model = module.GetStringDefault(config, "model", DEFAULT_MODEL)
+
+	return nil
+}
+
+func (am *AssistantManager) Start() error {
+	am.isRunning = true
+
+	return nil
+}
+
+func (am *AssistantManager) Stop() error {
+	am.isRunning = false
+
+	return nil
+}
+
+func (am *AssistantManager) IsRunning() bool {
+	return am.isRunning
+}
+
+func ()
