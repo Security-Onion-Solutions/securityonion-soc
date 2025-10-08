6efc9bc42e4cfa5360d43f0d7f89ea52bfc4bb7c
Matthew Wright <matthew.wright@securityonionsolutions.com>
2025-10-08 17:04:05 -0400

    first workings of the ai metrics page

---
+++ html/js/routes/aimetrics.js
@@ -0,0 +1,140 @@
+// Copyright 2019 Jason Ertel (github.com/jertel).
+// Copyright 2020-2025 Security Onion Solutions LLC and/or licensed to Security Onion Solutions LLC under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0 as shown at
+// https://securityonion.net/license; you may not use this file except in compliance with the
+// Elastic License 2.0.
+
+loadPageTemplate('page-aimetrics', 'pages/aimetrics.html');
+
+routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', component: {
+  template: '#page-aimetrics',
+  data() { return {
+    i18n: this.$root.i18n,
+    aimetrics: [],
+    headers: {
+      0: [
+        { title: this.$root.i18n.userId, value: 'userId' },
+        { title: this.$root.i18n.totalInputTokens, value: 'totalInputTokens' },
+        { title: this.$root.i18n.totalOutputTokens, value: 'totalOutputTokens' },
+        { title: this.$root.i18n.totalCredits, value: 'totalCredits' },
+        { title: this.$root.i18n.totalSessions, value: 'totalSessions' },
+        { title: this.$root.i18n.totalMessages, value: 'totalMessages' },
+      ],
+      1: [
+        { value: 'expand' },
+        { title: this.$root.i18n.id, value: 'id' },
+        { title: this.$root.i18n.createTime, value: 'createTime' },
+        { title: this.$root.i18n.sessionId, value: 'sessionId' },
+      ],
+      2: [
+        { value: 'expand' },
+        { title: this.$root.i18n.id, value: 'id' },
+        { title: this.$root.i18n.createTime, value: 'createTime' },
+      ],
+    },
+    dropdownFields: {
+      1: [
+        { title: this.$root.i18n.userId, value: 'userId' },
+        { title: this.$root.i18n.kind, value: 'kind' },
+        { title: this.$root.i18n.title, value: 'title' },
+        { title: this.$root.i18n.tags, value: 'tags' },
+        { title: this.$root.i18n.usage, value: 'usage' },
+      ],
+      2: [
+        { title: this.$root.i18n.userId, value: 'userId' },
+        { title: this.$root.i18n.kind, value: 'kind' },
+        { title: this.$root.i18n.sessionId, value: 'sessionId' },
+        { title: this.$root.i18n.message, value: 'message' },
+      ],
+    },
+    sortBy0: [{ key: 'totalCredits', order: 'desc' }],
+    sortBy1: [{ key: 'createTime', order: 'desc' }],
+    sortBy2: [{ key: 'createTime', order: 'desc' }],
+    itemsPerPage: 10,
+    itemsPerPageOptions: [10,50,250,1000],
+    tableSetting: 0, // 0: users 1: sessions 2: messages
+    expanded: [],
+  }},
+  created() {
+    this.loadData();
+  },
+  watch: {
+    '$route': 'loadData',
+    'sortBy0': 'saveLocalSettings',
+    'sortBy1': 'saveLocalSettings',
+    'sortBy2': 'saveLocalSettings',
+    'itemsPerPage': 'saveLocalSettings',
+  },
+  methods: {
+    async loadData() {
+      const currUserId = this.$route.params.userId;
+      const currSessionId = this.$route.params.sessionId;
+
+      this.$root.startLoading();
+      try {
+        if (currSessionId && currUserId) {
+          this.tableSetting = 2;
+          this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
+          const response = await this.$root.papi.get(`/assistant/admin/${currUserId}/sessions/${currSessionId}/history`, {
+            params: {
+              format: '2006/01/02 3:04:05 PM',
+              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+            }
+          });
+          this.aimetrics = response.data;
+        } else if (currUserId) {
+          this.tableSetting = 1;
+          this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
+          const response = await this.$root.papi.get(`/assistant/admin/${currUserId}/sessions`, {
+            params: {
+              format: '2006/01/02 3:04:05 PM',
+              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+            }
+          });
+          this.aimetrics = response.data;
+        } else {
+          this.tableSetting = 0;
+          this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
+          const response = await this.$root.papi.get('/assistant/admin/stats', {
+            params: {
+              format: '2006/01/02 3:04:05 PM',
+              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+            }
+          });
+          this.aimetrics = response.data;
+        }
+        this.loadLocalSettings();
+      } catch (error) {
+        this.$root.showError(error);
+        this.aimetrics = [];
+      }
+      this.$root.stopLoading();
+    },
+    saveLocalSettings() {
+      localStorage['settings.aimetrics.sortBy0'] = JSON.stringify(this.sortBy0);
+      localStorage['settings.aimetrics.sortBy1'] = JSON.stringify(this.sortBy1);
+      localStorage['settings.aimetrics.sortBy2'] = JSON.stringify(this.sortBy2);
+      localStorage['settings.aimetrics.itemsPerPage'] = this.itemsPerPage;
+    },
+    loadLocalSettings() {
+      if (localStorage['settings.aimetrics.sortBy0']) {
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy0']);  
+      }
+      if (localStorage['settings.aimetrics.sortBy1']) {
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy1']);  
+      }
+      if (localStorage['settings.aimetrics.sortBy2']) {
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy2']);  
+      }
+      if (localStorage['settings.aimetrics.itemsPerPage']) {
+        this.itemsPerPage = parseInt(localStorage['settings.aimetrics.itemsPerPage']);
+      }
+    },
+    buildUserLink(userId) {
+      return { name: 'aimetrics', params: { userId: userId } };
+    },
+    buildSessionLink(sessionId) {
+      return { name: 'aimetrics', params: { userId: this.$route.params.userId, sessionId: sessionId }}
+    },
+  }
+}});
