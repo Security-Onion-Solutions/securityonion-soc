2db02386d7e20757a38a5375a562ab3c4b9eea5b
Matthew Wright <matthew.wright@securityonionsolutions.com>
2025-10-10 12:28:23 -0400

    show date range filter, auto refresh option

--- html/js/routes/aimetrics.js
+++ html/js/routes/aimetrics.js
@@ -59,9 +59,50 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
       { title: "value", value: "value" }
     ],
     creditsRemaining: 0,
+    
+    // Date range filter properties
+    dateRange: '',
+    relativeTimeEnabled: true,
+    relativeTimeValue: 24,
+    relativeTimeUnit: RELATIVE_TIME_HOURS,
+    relativeTimeUnits: [],
+    zone: '',
+    autoRefreshInterval: 0,
+    autoRefreshIntervals: [],
+    autoRefreshTimer: null,
   }},
   created() {
+    this.relativeTimeUnits = [
+      { title: this.$root.i18n.seconds, value: RELATIVE_TIME_SECONDS },
+      { title: this.$root.i18n.minutes, value: RELATIVE_TIME_MINUTES },
+      { title: this.$root.i18n.hours, value: RELATIVE_TIME_HOURS },
+      { title: this.$root.i18n.days, value: RELATIVE_TIME_DAYS },
+      { title: this.$root.i18n.weeks, value: RELATIVE_TIME_WEEKS },
+      { title: this.$root.i18n.months, value: RELATIVE_TIME_MONTHS }
+    ];
+    this.autoRefreshIntervals = [
+      { title: this.$root.i18n.interval0s, value: 0 },
+      { title: this.$root.i18n.interval5s, value: 5 },
+      { title: this.$root.i18n.interval10s, value: 10 },
+      { title: this.$root.i18n.interval15s, value: 15 },
+      { title: this.$root.i18n.interval30s, value: 30 },
+      { title: this.$root.i18n.interval1m, value: 60 },
+      { title: this.$root.i18n.interval2m, value: 120 },
+      { title: this.$root.i18n.interval5m, value: 300 },
+      { title: this.$root.i18n.interval10m, value: 600 },
+      { title: this.$root.i18n.interval15m, value: 900 },
+      { title: this.$root.i18n.interval30m, value: 1800 },
+      { title: this.$root.i18n.interval1h, value: 3600 },
+      { title: this.$root.i18n.interval2h, value: 7200 },
+      { title: this.$root.i18n.interval5h, value: 18000 },
+      { title: this.$root.i18n.interval10h, value: 36000 },
+      { title: this.$root.i18n.interval24h, value: 86400 },
+    ];
+    this.zone = moment.tz.guess();
     this.loadData();
+  },
+  beforeUnmount() {
+    this.stopRefreshTimer();
   },
   watch: {
     '$route': 'loadData',
@@ -69,11 +110,45 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
     'sortBy1': 'saveLocalSettings',
     'sortBy2': 'saveLocalSettings',
     'itemsPerPage': 'saveLocalSettings',
+    'autoRefreshInterval': 'resetRefreshTimer',
   },
   methods: {
+    autoRefreshIntervalChanged() {
+      this.saveSetting('autoRefreshInterval', this.autoRefreshInterval, 0);
+    },
+    stopRefreshTimer() {
+      if (this.autoRefreshTimer) {
+        clearTimeout(this.autoRefreshTimer);
+      }
+    },
+    resetRefreshTimer() {
+      var route = this;
+      this.stopRefreshTimer();
+      if (this.autoRefreshInterval > 0) {
+        this.autoRefreshTimer = setTimeout(function () { route.loadData(); }, this.autoRefreshInterval * 1000);
+      }
+    },
+    saveSetting(name, value, defaultValue = null) {
+      var item = 'settings.aimetrics.' + name;
+      if (defaultValue == null || value != defaultValue) {
+        localStorage[item] = value;
+      } else {
+        localStorage.removeItem(item);
+      }
+    },
     async loadData() {
       const currUserId = this.$route.params.userId;
       const currSessionId = this.$route.params.sessionId;
+      
+      this.loadLocalSettings();
+      //debugger;
+      // Calculate date range
+      let range = this.dateRange;
+      if (this.relativeTimeEnabled || range === '') {
+        this.dateRange = '';
+        range = this.getStartDate().format(this.$root.i18n.timePickerFormat) + " - " + this.getEndDate().format(this.$root.i18n.timePickerFormat);
+        this.dateRange = range;
+      }
 
       this.$root.startLoading();
       try {
@@ -82,8 +157,9 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
           this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
           const response = await this.$root.papi.get(`/assistant/admin/${currUserId}/sessions/${currSessionId}/history`, {
             params: {
-              format: '2006/01/02 3:04:05 PM',
-              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+              format: this.$root.i18n.timePickerSample,
+              range: range,
+              timezone: this.zone,
             }
           });
           this.aimetrics = response.data;
@@ -92,8 +168,9 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
           this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
           const response = await this.$root.papi.get(`/assistant/admin/${currUserId}/sessions`, {
             params: {
-              format: '2006/01/02 3:04:05 PM',
-              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+              format: this.$root.i18n.timePickerSample,
+              range: range,
+              timezone: this.zone,
             }
           });
           this.aimetrics = response.data;
@@ -102,38 +179,55 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
           this.$root.adjustSubgridColVisibility(this.headers[this.tableSetting]);
           const response = await this.$root.papi.get('/assistant/admin/stats', {
             params: {
-              format: '2006/01/02 3:04:05 PM',
-              range: '2000/01/01 00:00:00 AM - 2050/01/01 00:00:00 AM',
+              format: this.$root.i18n.timePickerSample,
+              range: range,
+              timezone: this.zone,
             }
           });
           this.aimetrics = response.data;
         }
-        this.loadLocalSettings();
       } catch (error) {
         this.$root.showError(error);
         this.aimetrics = [];
       }
       this.$root.stopLoading();
       await this.loadCredits();
+      this.resetRefreshTimer();
     },
     saveLocalSettings() {
-      localStorage['settings.aimetrics.sortBy0'] = JSON.stringify(this.sortBy0);
-      localStorage['settings.aimetrics.sortBy1'] = JSON.stringify(this.sortBy1);
-      localStorage['settings.aimetrics.sortBy2'] = JSON.stringify(this.sortBy2);
-      localStorage['settings.aimetrics.itemsPerPage'] = this.itemsPerPage;
+      this.saveSetting('sortBy0', JSON.stringify(this.sortBy0), '[]');
+      this.saveSetting('sortBy1', JSON.stringify(this.sortBy1), '[]');
+      this.saveSetting('sortBy2', JSON.stringify(this.sortBy2), '[]');
+      this.saveSetting('itemsPerPage', this.itemsPerPage, 10);
+      this.saveSetting('relativeTimeValue', this.relativeTimeValue, 24);
+      this.saveSetting('relativeTimeUnit', this.relativeTimeUnit, RELATIVE_TIME_HOURS);
+      this.saveSetting('autoRefreshInterval', this.autoRefreshInterval, 0);
+      localStorage['timezone'] = this.zone;
     },
     loadLocalSettings() {
       if (localStorage['settings.aimetrics.sortBy0']) {
-        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy0']);  
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy0']);
       }
       if (localStorage['settings.aimetrics.sortBy1']) {
-        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy1']);  
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy1']);
       }
       if (localStorage['settings.aimetrics.sortBy2']) {
-        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy2']);  
+        this.sortBy = JSON.parse(localStorage['settings.aimetrics.sortBy2']);
       }
       if (localStorage['settings.aimetrics.itemsPerPage']) {
         this.itemsPerPage = parseInt(localStorage['settings.aimetrics.itemsPerPage']);
+      }
+      if (localStorage['settings.aimetrics.relativeTimeValue']) {
+        this.relativeTimeValue = parseInt(localStorage['settings.aimetrics.relativeTimeValue']);
+      }
+      if (localStorage['settings.aimetrics.relativeTimeUnit']) {
+        this.relativeTimeUnit = parseInt(localStorage['settings.aimetrics.relativeTimeUnit']);
+      }
+      if (localStorage['timezone']) {
+        this.zone = localStorage['timezone'];
+      }
+      if (localStorage['settings.aimetrics.autoRefreshInterval']) {
+        this.autoRefreshInterval = parseInt(localStorage['settings.aimetrics.autoRefreshInterval']);
       }
     },
     buildUserLink(userId) {
@@ -181,6 +275,75 @@ routes.push({ path: '/aimetrics/:userId?/:sessionId?', name: 'aimetrics', compon
         }
       }
       return data;
+    },
+    // Date range methods
+    getEndDate() {
+      if (this.dateRange != '') {
+        var pieces = this.dateRange.split(" - ");
+        if (pieces.length == 2) {
+          return moment(pieces[1], this.$root.i18n.timePickerFormat);
+        }
+      }
+      return moment();
+    },
+    getStartDate() {
+      if (this.dateRange != '') {
+        var pieces = this.dateRange.split(" - ");
+        if (pieces.length == 2) {
+          return moment(pieces[0], this.$root.i18n.timePickerFormat);
+        }
+      }
+      var unit = "hour";
+      switch (this.relativeTimeUnit) {
+        case RELATIVE_TIME_SECONDS: unit = "seconds"; break;
+        case RELATIVE_TIME_MINUTES: unit = "minutes"; break;
+        case RELATIVE_TIME_HOURS: unit = "hours"; break;
+        case RELATIVE_TIME_DAYS: unit = "days"; break;
+        case RELATIVE_TIME_WEEKS: unit = "weeks"; break;
+        case RELATIVE_TIME_MONTHS: unit = "months"; break;
+      }
+      return moment().subtract(this.relativeTimeValue, unit);
+    },
+    setupDateRangePicker() {
+      if (this.relativeTimeEnabled) return;
+
+      $('#aimetricsdaterange').daterangepicker({
+        ranges: this.$root.generateDatePickerPreselects(),
+        timePicker: true,
+        timePickerSeconds: true,
+        endDate: this.getEndDate(),
+        startDate: this.getStartDate(),
+        locale: {
+          format: this.$root.i18n.timePickerFormat
+        }
+      });
+      var route = this;
+      if (route.dateRange == '') {
+        route.dateRange = $('#aimetricsdaterange')[0].value;
+      }
+      $('#aimetricsdaterange').on('hide.daterangepicker', function (ev, picker) {
+        route.hideDateRangePicker();
+      });
+    },
+    showDateRangePicker() {
+      if (this.relativeTimeEnabled) return;
+      $('#aimetricsdaterange').click();
+    },
+    hideDateRangePicker() {
+      if (this.relativeTimeEnabled) return;
+      this.dateRange = $('#aimetricsdaterange')[0].value;
+      this.loadData();
+    },
+    showAbsoluteTime() {
+      this.relativeTimeEnabled = false;
+      setTimeout(this.setupDateRangePicker, 10);
+    },
+    showRelativeTime() {
+      this.relativeTimeEnabled = true;
+    },
+    notifyInputsChanged() {
+      this.saveLocalSettings();
+      this.loadData();
     },
   }
 }});
