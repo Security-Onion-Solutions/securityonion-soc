57f8e84c60b4edf6e8002333ddfae57dd088b618
Jason Ertel <jason.ertel@securityonionsolutions.com>
2026-04-14 17:21:50 -0400

    use license URL if available

--- server/modules/assistant/soai_adapter_test.go
+++ server/modules/assistant/soai_adapter_test.go
@@ -111,6 +111,21 @@ func TestSOAiConstructor(t *testing.T) {
 	}
 }
 
+func TestSOAiConstructorWithLicense(t *testing.T) {
+	// a test key with aiGatewayUrl = https://license.api.com
+	license := `H4sIAAAAAAAA/zyS246qSBiFX6VTt9hbEEoOd4B4QBHUEm0nk50CCizlJFWAOJl3n9g92cl/8a/kW+vq+weQNCUxpx0BBpiIE/gpwk8JIlE0vu8CRoA8a9pgTqvyDzP9lBQ0kQ2oG1B/MyUu3gsHErcN5cOHX9Kq/AiaCowATYABsgbHlPzmhPEcR7/fMyKUIBiBnMakZORdX3xDH+gHAiOQEszbhjBg/PX3CLSMNAwY4giUVUJ+PlbFxyYHBgAjkGCO/wRGs/K7DAywjnk3CBd5+UqKaTDTVbPG4WtQeNF2R/do545yuu8O0xJu9cWSrOuid8y5j+ZbiP0kdVCXjXfS/u7zvu5e4gnafgbbbfacdMJwmj0b7a6u/F4e+ph163VJXdkUXXa91HYg9aSQVy1eHWC9cNKedt1hSyVhU1gybl8bSw33ar6cp9XMwdou60+zR8OklJrQlS2PTptnciz2tlBaQyl8LexqqqbCbesJiWaen2ii7jCCTyFOtppdIqI+be6JK8uMrGiuU0FbwPu9QOl1rpuWFSvB2RfG1/MXYo6tLBzGAnPzfCxFORtacsa9NihSuFFfyH3o/nFQvfV+jw8mUbhTeTPS3rTbccqOfV7kbe61m0EKZ2vN3ouWHHWC6sa+n8mcvlbX9pB0blwseaPnm/E4mNleGE0dF9WRi5M+Yxekh8Jkp4TzYDFDqx3WJ1v6eChKr7y6ZqV04dK0BaeJo2OILqI0XL40T1VJHZwPkdYpSgUriyEPXq4n1PgWFFsmzR0t8tp4d9i7YnHb3fhaDsAIYLrAnPR4+BHkynnNjPH4f/l+4Zr+iqsC/PtfAAAA//+k/vXDEgMAAA==`
+	licensing.Init(license)
+
+	srv := &server.Server{}
+	config := map[string]interface{}{}
+	adapter, err := NewSOAiCloudAdapter(context.Background(), srv, config)
+
+	assert.NoError(t, err)
+	br := adapter.(*SOAiCloudAdapter)
+	assert.Equal(t, "https://license.api.com", br.apiUrl)
+}
+
+
 func TestSOAiGetHealth(t *testing.T) {
 	// an expired test key for buildApiKey
 	license := `H4sIAAziJWgC/z2NW4+iQBSE/8qEV2YiCC3gGzcvKALSoONms2mgQZSbNFc3+98XZ5JJ6qFO6qs6fykcxzhs0g5TS2rOzMEHAz5YABlm+aUL9U7hoUpr1KRl8cMsPlgezrklkCa9mALlrwUXh22dNuObVUz4m12XU5ZGU5LUKEzxnwaTJkPBn9cMA1gwxVka4oLgV339Bb3Bb2jKYoyatsaEWv76/U61BNeTZaZ3ZYS/HSlDr86m7kRHqEE/B0mT4qs8nbuw6Ub6wm2eUb6wNUmQK+Q/R77J284zPDXT+dPdcRcFOEjrDd5Vea/LKwuuDgBZUazDLpk57PFuNX3VPZkTUK0EtIdkmHf0eNKGWrwLW6vnxj4k3W5XpAYnMwa5XirVZnucc9sWbV1QrfW4T7vOPaQsvc8VDrXPvSL4RyHbrOJS05HoJP1Je9SEjVMZGJxipot6iLz8qNKFMhb051otF0JM3w4mHYnyeYBzwUEQDHQYHUS1gFgY1MZktoocKMFKSmlxDe73HMbXlSQrSsjbZ4ueXc+fkOgqv9YJseX98NgwXDK2+Ix6ceRZfy88ofGQLG8UzN3xiFwZ841emhpub+LNWxCvz/Kszcx2P7K+thPVI6NwQUcLRmhZCdekz+21daPOCPNNU0vZfjazNdX0g4VuwCowUNQn5AIln547vL+y1xrcOkiaH9LHg+d7/tnVW77zN7JK63UYeD68MOx4+RRNQcCVfXYDseP5EpQKgSa4XE+wthTAtIRd6WJgtqHjHg0mvzm3ZsfZ1L//d3LBEekCAAA=`
