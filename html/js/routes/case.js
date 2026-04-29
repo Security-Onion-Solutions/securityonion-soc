9955ab5010790cc16f441b451eae5c425b0f4116
Corey Ogburn <corey.ogburn@securityonionsolutions.com>
2026-04-29 12:18:50 -0600

    Componentize Columns for 1.3.2
    On the case and detection pages we used a pattern for small screen devices that breaks WCAG 2.1 SC 1.3.2 Level A by using the `order` properties on v-col to rearrange the primary detail column and the summary column on mobile screens. To achieve the same effect but meet the SC, I turned each column into a component and reuse the component before and after the details column and only show one or the other depending on screen size. Small screens will continue to show summary before details each being full-width, large screens continue to show details to the left of the summary column, and screen readers get a UI that's rendered in the same order as the DOM.

--- html/js/routes/case.js
+++ html/js/routes/case.js
@@ -511,6 +511,20 @@ routes.push({ path: '/case/:id', name: 'case', component: {
       return form;
     },
 
+    async onDetailsSave({ field, value }) {
+      const savedEditForm = this.editForm;
+      this.editForm = {
+        valid: true,
+        field: field,
+        val: value,
+        orig: this.caseObj[field],
+      };
+      try {
+        await this.modifyCase();
+      } finally {
+        this.editForm = savedEditForm;
+      }
+    },
     async modifyCase() {
       let success = false;
       this.$root.startLoading();
@@ -1049,7 +1063,6 @@ routes.push({ path: '/case/:id', name: 'case', component: {
       });
       return hours;
     },
-
     saveLocalSettings() {
       localStorage['settings.case.mruCases'] = JSON.stringify(this.mruCases);
     },
